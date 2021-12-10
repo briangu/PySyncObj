8dd8b13d6ed39161cf479903ba465265cace3e30
Brian Guarraci <brian.guarraci@gmail.com>
2021-12-10 14:11:30 -0700

    show how to return lock status in tornado

--- examples/lock.py
+++ examples/lock.py
@@ -5,33 +5,18 @@ import sys
 import threading
 import weakref
 import time
+
 sys.path.append("../")
 from pysyncobj import SyncObj, replicated, SyncObjConf
 import asyncio
 import sys
-# import tornado
-import tornado.ioloop
 import tornado.web
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        # print("here!")
-        self.write("Hello, world")
-
-def make_app():
-    print("making app")
-    return tornado.web.Application([
-        (r"/", MainHandler),
-    ])
-
-# if __name__ == "__main__":
-#     app = make_app()
-#     app.listen(8888)
-#     tornado.ioloop.IOLoop.current().start()
 
 class LockImpl(SyncObj):
     def __init__(self, selfAddress, partnerAddrs, autoUnlockTime, conf):
         super(LockImpl, self).__init__(selfAddress, partnerAddrs, conf=conf)
+        self.__selfClientID = selfAddress
         self.__locks = {}
         self.__autoUnlockTime = autoUnlockTime
         self.__verbose = True
@@ -39,7 +24,7 @@ class LockImpl(SyncObj):
     @replicated
     def acquire(self, lockPath, clientID, currentTime):
         if self.__verbose:
-            print(f"{threading.get_ident()} lock: {lockPath}, {clientID}, {currentTime}")
+            print(f"{threading.get_ident()} acquire: {lockPath}, {clientID}, {currentTime}")
         existingLock = self.__locks.get(lockPath, None)
         # Auto-unlock old lock
         if existingLock is not None:
@@ -54,8 +39,8 @@ class LockImpl(SyncObj):
 
     @replicated
     def ping(self, clientID, currentTime):
-        if self.__verbose:
-            print(f"ping: {clientID}, {currentTime}, {self.__locks}")
+        # if self.__verbose:
+        #     print(f"ping: {clientID}, {currentTime}, {self.__locks}")
         for lockPath in list(self.__locks.keys()):
             lockClientID, lockTime = self.__locks[lockPath]
 
@@ -129,21 +114,22 @@ class Lock(object):
 
     def isAcquired(self, path):
         return self.__lockImpl.isAcquired(path, self.__selfID, time.time())
-    
+
     def isOwned(self, path):
         return self.__lockImpl.isOwned(path, self.__selfID, time.time())
 
     def release(self, path):
         self.__lockImpl.release(path, self.__selfID)
 
-    def printStatus(self):
-        self.__lockImpl._printStatus()
+    def getStatus(self):
+        return self.__lockImpl.getStatus()
 
     def toggle_verbose(self):
         self.__lockImpl.toggle_verbose()
 
-    def onTick(self, timeToWait):
+    def onTick(self):
         self.__lockImpl._onTick(timeToWait=0)
+
 
 def printHelp():
     print('')
@@ -159,56 +145,6 @@ def printHelp():
     print('')
 
 
-def main():
-    if len(sys.argv) < 3:
-        print('Usage: %s selfHost:port partner1Host:port partner2Host:port ...' % sys.argv[0])
-        sys.exit(-1)
-
-    selfAddr = sys.argv[1]
-    partners = sys.argv[2:]
-
-    conf = SyncObjConf(autoTick=False)
-
-    lock = Lock(selfAddr, partners, 10.0, conf=conf)
-
-    def get_input(v):
-        if sys.version_info >= (3, 0):
-            return input(v)
-        else:
-            return raw_input(v)
-
-    def lock_status(path):
-        if lock.isOwned(path):
-            return "owned"
-        elif lock.isAcquired(path):
-            return "acquired"
-        return "released"
-
-    printHelp()
-    while True:
-        cmd = get_input(f"{threading.get_ident()} >> ").split()
-        if not cmd:
-            continue
-        elif cmd[0] == 'help':
-            printHelp()
-        elif cmd[0] == 'check':
-            print(lock_status(cmd[1]))
-        elif cmd[0] == 'acquire':
-            lock.tryAcquireLock(cmd[1])
-            time.sleep(1.5)
-            print(lock_status(cmd[1]))
-        elif cmd[0] == 'release':
-            lock.release(cmd[1])
-            time.sleep(1.5)
-            print(lock_status(cmd[1]))
-        elif cmd[0] == 'status':
-            lock.printStatus()
-        elif cmd[0] == 'verbose':
-            lock.toggle_verbose()
-
-
-
-
 async def connect_stdin_stdout():
     loop = asyncio.get_event_loop()
     reader = asyncio.StreamReader()
@@ -217,6 +153,7 @@ async def connect_stdin_stdout():
     w_transport, w_protocol = await loop.connect_write_pipe(asyncio.streams.FlowControlMixin, sys.stdout)
     writer = asyncio.StreamWriter(w_transport, w_protocol, reader, loop)
     return reader, writer
+
 
 # https://stackoverflow.com/questions/45419723/python-timer-with-asyncio-coroutine
 class Timer:
@@ -234,36 +171,28 @@ class Timer:
         self._task.cancel()
 
 
-async def main():
+async def main(sync_lock):
     reader, writer = await connect_stdin_stdout()
 
     if len(sys.argv) < 3:
         print('Usage: %s selfHost:port partner1Host:port partner2Host:port ...' % sys.argv[0])
         sys.exit(-1)
 
-    selfAddr = sys.argv[1]
-    partners = sys.argv[2:]
-
-    conf = SyncObjConf(autoTick=False)
-
-    lock = Lock(selfAddr, partners, 10.0, conf=conf)
-
-    def get_input(v):
-        if sys.version_info >= (3, 0):
-            return input(v)
-        else:
-            return raw_input(v)
+    # def get_input(v):
+    #     if sys.version_info >= (3, 0):
+    #         return input(v)
+    #     else:
+    #         return raw_input(v)
 
     def lock_status(path):
-        if lock.isOwned(path):
+        if sync_lock.isOwned(path):
             return "owned"
-        elif lock.isAcquired(path):
+        elif sync_lock.isAcquired(path):
             return "acquired"
         return "released"
 
     async def timeout_callback():
-        lock.onTick(conf.autoTickPeriod)
-        # print("ontick")
+        sync_lock.onTick()
 
     # timer = Timer(conf.autoTickPeriod*2, timeout_callback)
     timer = Timer(2, timeout_callback)
@@ -277,7 +206,7 @@ async def main():
         if not res:
             break
 
-        cmd = res.split() #get_input(f"{threading.get_ident()} >> ").split()
+        cmd = res.split()  # get_input(f"{threading.get_ident()} >> ").split()
         # writer.write(cmd.encode('utf-8'))
         if not cmd:
             continue
@@ -286,48 +215,62 @@ async def main():
         elif cmd[0] == 'check':
             writer.write(lock_status(cmd[1]).encode('utf-8'))
         elif cmd[0] == 'acquire':
-            lock.tryAcquireLock(cmd[1])
+            sync_lock.tryAcquireLock(cmd[1])
             time.sleep(1.5)
             writer.write(lock_status(cmd[1]).encode('utf-8'))
         elif cmd[0] == 'release':
-            lock.release(cmd[1])
+            sync_lock.release(cmd[1])
             time.sleep(1.5)
             writer.write(lock_status(cmd[1]).encode('utf-8'))
         elif cmd[0] == 'status':
-            lock.printStatus()
+            print(sync_lock.getStatus())
         elif cmd[0] == 'verbose':
-            lock.toggle_verbose()
+            sync_lock.toggle_verbose()
 
-        # writer.write(res)
 
-# async def main():
-#     while True:
-#         await asyncio.sleep(1)
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+
+class StatusHandler(tornado.web.RequestHandler):
+    sync_lock = None
+
+    def initialize(self, sync_lock):
+        self.sync_lock = sync_lock
+
+    def get(self):
+        self.write(str(self.sync_lock.getStatus()))
+
+
+def make_app(sync_lock):
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/status", StatusHandler, {'sync_lock': sync_lock}),
+    ])
 
 
 if __name__ == "__main__":
 
-    import tornado.platform.asyncio
+    selfAddr = sys.argv[1]
+    partners = sys.argv[2:]
 
-    tornado.platform.asyncio.AsyncIOMainLoop().install()
+    conf = SyncObjConf(autoTick=False)
 
-    app = make_app()
+    lock = Lock(selfAddr, partners, 10.0, conf=conf)
+
+    app = make_app(lock)
     selfAddr = int(sys.argv[1].split(":")[1]) + 1000
     print(f"port: {selfAddr}")
     app.listen(selfAddr)
-    # tornado.ioloop.IOLoop.current().start()
     print('here again')
-    # asyncio.get_event_loop().run_forever()
-    # asyncio.run(main())
 
     loop = asyncio.get_event_loop()
-    # asyncio.set_event_loop(loop)
     try:
-        loop.run_until_complete(main())
+        loop.run_until_complete(main(lock))
     finally:
         loop.run_until_complete(loop.shutdown_asyncgens())
         loop.close()
 
 # if __name__ == '__main__':
 #     main()
-
